64483b9d994098e75166ad240bcb3c24fb55a480
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-05-23 22:01:45 +0300

    sprite

--- src/components/types/Icon.tsx
+++ src/components/types/Icon.tsx
@@ -6,7 +6,7 @@ interface IconProps {
 const Icon: React.FC<IconProps> = ({ name, className }) => {
   return (
     <svg className={`inline-block fill-current ${className}`}>
-      <use xlinkHref={`/images/sprite.svg#${name}`} />
+      <use xlinkHref={`${import.meta.env.BASE_URL}images/sprite.svg#${name}`} />
     </svg>
   );
 };
