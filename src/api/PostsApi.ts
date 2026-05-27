64479cc2bf69e5f5359c766f2b39299879c571ac
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-05-27 16:47:22 +0300

    serve

--- src/api/PostsApi.ts
+++ src/api/PostsApi.ts
@@ -4,8 +4,8 @@ import { CreatePostInput } from "@/schemas/CreatePostSchema";
 import { BASE_URL } from "./config";
 
 export const getPosts = async (): Promise<IPost[]> => {
-  // const response = await fetch('/api/posts');
-  const response = await fetch(`${BASE_URL}/api/posts`);
+  const response = await fetch('/api/posts');
+  // const response = await fetch(`${BASE_URL}/api/posts`);
 
   if (!response.ok) {
     throw new Error("Не удалось загрузить истории");
@@ -34,8 +34,8 @@ export const createPostFetch = async (
   formData.append("city", data.city);
   formData.append("photo", data.photo);
 
-  // const response = await fetch("/api/posts", {
-  const response = await fetch(`${BASE_URL}/api/posts`, {
+  const response = await fetch("/api/posts", {
+  // const response = await fetch(`${BASE_URL}/api/posts`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${token}`,
--- vite.config.js
+++ vite.config.js
@@ -6,48 +6,52 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ["import"],
-        additionalData: `@use "@/assets/styles/global/_variables.scss" as *;
-         @use "@/assets/styles/global/_mixins.scss" as *;
-         @use "@/assets/styles/global/_btn.scss" as *;`,
-      },
-    },
-  },
-  plugins: [react()],
-  build: {
-    cssMinify: "esbuild", 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor"; 
-          }
+// Переводим конфиг на функцию, чтобы получить доступ к режиму (command)
+export default defineConfig(({ command }) => {
+  return {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ["import"],
+          additionalData: `@use "@/assets/styles/global/_variables.scss" as *;
+           @use "@/assets/styles/global/_mixins.scss" as *;
+           @use "@/assets/styles/global/_btn.scss" as *;`,
         },
       },
     },
-  },
-  base: "/Travel__blog/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://travelblog.skillbox.cc",
-        changeOrigin: true,
-        secure: false,
+    plugins: [react()],
+    build: {
+      cssMinify: "esbuild", 
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes("node_modules")) {
+              return "vendor"; 
+            }
+          },
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  
+    base: command === "serve" ? "/" : "/Travel__blog/",
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://travelblog.skillbox.cc",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    css: true,
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      css: true,
+    },
+  };
 });
